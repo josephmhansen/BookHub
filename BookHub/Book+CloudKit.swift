92b1bf1c49daaeffe5f092cc36abd74767818e0e
Diego-F-Aguirre <diego3884@gmail.com>
2016-08-10 14:51:35 -0600

    Model updated to use CKAsset to store image

--- BookHub/Book+CloudKit.swift
+++ BookHub/Book+CloudKit.swift
@@ -13,27 +13,42 @@ import CloudKit
 extension Book {
     static var recordType: String { return "Book" }
     static var ratingKey: String { return "Rating" }
-    static var imageKey: String { return "Image" }
+    static var imageDataKey: String { return "ImageData" }
     static var dateKey: String { return "Date" }
     
     init?(ckRecord: CKRecord) {
-        guard let rating = ckRecord[Book.ratingKey] as? String,
-            image = ckRecord[Book.imageKey] as? NSData,
-            date = ckRecord[Book.dateKey] as? NSDate
+        guard let timeStamp = ckRecord.creationDate,
+            photoAsset = ckRecord[Book.imageDataKey] as? CKAsset,
+            rating = ckRecord[Book.ratingKey] as? String
             where ckRecord.recordType == Book.recordType else {
-                print("Failable Init failed")
+                print("Failable initializer Failed!!!")
                 return nil
         }
         
-        self.init(rating: rating, date: date, photoData: image)
+        let photoData = NSData(contentsOfURL: photoAsset.fileURL)
+        
+        self.init(rating: rating, date: timeStamp, photoData: photoData)
+    }
+    
+    private var temporaryPhotoURL: NSURL {
+        
+        // Must write to temporary directory to be able to pass image file path url to CKAsset
+        
+        let temporaryDirectory = NSTemporaryDirectory()
+        let temporaryDirectoryURL = NSURL(fileURLWithPath: temporaryDirectory)
+        let fileURL = temporaryDirectoryURL.URLByAppendingPathComponent(NSUUID().UUIDString).URLByAppendingPathExtension("jpg")
+        
+        photoData?.writeToURL(fileURL, atomically: true)
+        
+        return fileURL
     }
     
     var cloudKitRecord: CKRecord {
         let record = CKRecord(recordType: Book.recordType)
         
-        record[Book.ratingKey] = rating
         record[Book.dateKey] = date
-        record[Book.imageKey] = photoData
+        record[Book.ratingKey] = rating
+        record[Book.imageDataKey] = photoData
         
         return record
     }
