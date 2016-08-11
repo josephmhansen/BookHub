e020e4c1b91591acaf6eb5d24d45b56f948b2903
Diego-F-Aguirre <diego3884@gmail.com>
2016-08-11 00:42:58 -0600

    Updated Model to match Timeline’s model
    - The Book model is now a class that conforms to CloudKitSyncable
    - Removed Book+CloudKit file and included all information in the original Book model

--- BookHub/Book.swift
+++ BookHub/Book.swift
@@ -8,13 +8,63 @@
 
 import Foundation
 import UIKit
+import CloudKit
 
-struct Book {
+class Book: CloudKitSyncable {
+    
+    static let recordType = "Book"
+    static let ratingKey = "Rating"
+    static let photoDataKey = "ImageData"
+    static let dateKey = "Date" 
+    
     var rating: String
-    let date: NSDate
+    let timeStamp: NSDate
     var photoData: NSData?
     var photo: UIImage? {
         guard let photoData = photoData else { return nil }
         return UIImage(data: photoData)
+    }
+    
+    var cloudKitRecordID: CKRecordID?
+    var recordType: String { return Book.recordType }
+    
+    init(rating: String, timestamp: NSDate = NSDate(), photoData: NSData?) {
+        self.rating = rating
+        self.timeStamp = timestamp
+        self.photoData = photoData
+    }
+    
+    convenience required init?(record: CKRecord) {
+        
+        guard let rating = record[Book.ratingKey] as? String,
+            timestamp = record.creationDate,
+            photoAsset = record[Book.photoDataKey] as? CKAsset else { return nil }
+        
+        let photoData = NSData(contentsOfURL: photoAsset.fileURL)
+        self.init(rating: rating, timestamp: timestamp, photoData: photoData)
+        cloudKitRecordID = record.recordID
+    }
+    
+    private var temporaryPhotoURL: NSURL {
+        
+        // Must write to temporary directory to be able to pass image file path url to CKAsset
+        
+        let temporaryDirectory = NSTemporaryDirectory()
+        let temporaryDirectoryURL = NSURL(fileURLWithPath: temporaryDirectory)
+        let fileURL = temporaryDirectoryURL.URLByAppendingPathComponent(NSUUID().UUIDString).URLByAppendingPathExtension("jpg")
+        
+        photoData?.writeToURL(fileURL, atomically: true)
+        
+        return fileURL
+    }
+}
+
+extension CKRecord {
+    convenience init(_ book: Book) {
+        let recordID = CKRecordID(recordName: NSUUID().UUIDString)
+        self.init(recordType: book.recordType, recordID: recordID)
+        
+        self[Book.dateKey] = book.timeStamp
+        self[Book.photoDataKey] = CKAsset(fileURL: book.temporaryPhotoURL)
     }
 }
---
+++ BookHub/CloudKitSyncable.swift
@@ -0,0 +1,32 @@
+//
+//  CloudKitSyncable.swift
+//  BookHub
+//
+//  Created by Diego Aguirre on 8/10/16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+protocol CloudKitSyncable {
+    
+    init?(record: CKRecord)
+    
+    var cloudKitRecordID: CKRecordID? { get set }
+    var recordType: String { get }
+}
+
+extension CloudKitSyncable {
+    var isSynced: Bool {
+        return cloudKitRecordID != nil
+    }
+    
+    var cloudKitReference: CKReference? {
+        
+        guard let recordID = cloudKitRecordID else { return nil }
+        
+        return CKReference(recordID: recordID, action: .None)
+    }
+}
+
